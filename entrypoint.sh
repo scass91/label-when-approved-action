f1199e4f8ca413825a7c9d2f862660a6cc20f9cf
Simon Cass <scass1991@gmail.com>
2020-01-17 16:26:55 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -68,9 +68,7 @@ update_label() {
   done
 }
 
-if [[ "$action" == "submitted" ]] && [[ "$state" == "approved" ]]; then
-  update_label
-elif [[ "$action" == "dismissed" ]] && [[ "$state" == "dismissed" ]]; then
+if [[ "$action" == "submitted" ]] && [[ "$state" == "approved" ]] || [[ "$action" == "dismissed" ]] && [[ "$state" == "dismissed" ]]; then
   update_label
 else
   echo "Ignoring event ${action}/${state}"
